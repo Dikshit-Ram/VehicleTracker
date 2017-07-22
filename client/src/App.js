404774dbed66138ee7cd88c9207009d2d92942c9
dikshit ram <dikshit.ram02@gmail.com>
2017-07-22 00:52:22 -0400

    feat(module-api): add module client

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './App.css';
+import VehicleApp from './Components/Vehicle/VehicleApp';
+
+
+class App extends Component {
+    render() {
+
+        console.log("into app")
+        return (
+            < div
+        className = "App" >
+
+            < div
+        className = "App-header" >
+            < h2 > Vehicle
+        List < / h2 >
+        < / div >
+
+        < VehicleApp / >
+
+        < / div >
+
+
+    )
+        ;
+    }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+/*import {Router,Route,IndexRoute,Link} from 'react-router';*/
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Details from './Components/Views/Details';
+
+
+ReactDOM.render((
+
+    < BrowserRouter >
+
+    < Switch >
+    < Route
+exact
+path = '/'
+component = {App} / >
+
+    < Route
+path = '/list/:number'
+component = {Details} / >
+
+
+    < / Switch >
+    < / BrowserRouter >
+),
+document.getElementById('root')
+)
+;
+registerServiceWorker();
